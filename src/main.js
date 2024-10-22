c44f15fde5734ad6d0b61d638bf659858df29f61
Asafe-C <asafecalebe@outlook.com>
2024-10-22 18:33:08 -0300

    FEAT: Base 3.0 (acho que é a ultima vez que precisa mexer na base)

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
