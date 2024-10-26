ebd1688c97e15d1dc8933ef4cccde7ec51c3280d
Asafe-C <asafecalebe@outlook.com>
2024-10-26 16:28:51 -0300

    ADD + FEAT: restruturação da pag principal e adição do arquivo base para criação do componente

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,31 +24,95 @@
     </nav>
     <main>
       <section class="dest">
-        <div class="destaques">
-          <h1 style="text-align: center;">Produtos em Destaque</h1>
-          <br>
-          <b-carousel
-            id="destaquesCarrossel"
-            style="text-shadow: 0px 0px 2px #000;"
-            fade
-            :interval="3500"
-            indicators
-            img-width="1024"
-            img-height="480"
-          >
-            <b-carousel-slide :img-src="dest1">
-              <h1>Funko-Pop Deadpool</h1>
-            </b-carousel-slide>
-            <b-carousel-slide :img-src="dest2">
-              <h1>EA Sports FC - 2024</h1>
-            </b-carousel-slide>
-          </b-carousel>
+        <div class="expansao">
+          <div class="destaques">
+            <h1 style="text-align: center;">Produtos em Destaque</h1>
+            <br>
+            <b-carousel
+              id="destaquesCarrossel"
+              fade
+              :interval="3500"
+              indicators
+              img-width="1024"
+              img-height="480"
+            >
+              <b-carousel-slide :img-src="dest1"></b-carousel-slide>
+              <b-carousel-slide :img-src="dest2"></b-carousel-slide>
+            </b-carousel>
+          </div>
         </div>
       </section>
+      <section>
+        <h1 style="text-align: center;">Recomendados</h1>
+        <br>
+        <CarrosselDivs></CarrosselDivs>
+      </section>
+      <section>
+        <h1 style="text-align: center;">Produtos com Desconto</h1>
+        <br>
+        
+      </section>
     </main>
+    <footer>
+      <!-- Pagamentos -->
+      <h4>Formas de Pagamento</h4>
+      <div class="imgF">
+        <img src="../images/footer/visa.png" alt="Visa" class="if" />
+        <img src="../images/footer/mastercard_vrt_pos_92px_2x.png" alt="Mastercard" class="if" />
+        <img src="../images/footer/Banco_Itaú_logo.svg.png" alt="Itaú" class="if" />
+        <img src="../images/footer/logo-pix-520x520.png" alt="Pix" class="if" />
+      </div>
+      <!-- Redes Sociais -->
+      <h4>Redes Sociais</h4>
+      <div class="imgF">
+        <a href=""><img src="../images/footer/face.png" alt="Twitter" class="if" /></a>
+        <a href="https://www.instagram.com/escuderia.nexus?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw== "><img
+            src="../images/footer/Instagram_icon.webp" alt="Instagram" class="if" /></a>
+      </div>
+      <!-- Atendimento -->
+      <h4>Atendimento</h4>
+      <div class="imgF">
+        <a href=""><img src="../images/footer/whatsapp.png" alt="(81) 90000-0000" class="if" /></a>
+        <a href=""><img src="../images/footer/gmail.png" alt="Nexus69@gmail.com" class="if" /></a>
+      </div>
+      <br />
+      <p style="font-size: 115%">
+        &copy; Todos os Direitos Reservados a Equipe Nexus - Sesi Ibura 2024
+      </p>
+    </footer>
   </body>
 </template>
 
+
+<style lang="sass">
+  #destaquesCarrossel
+    .carousel-indicators li
+      background-color: black
+    .carousel-indicators li.active
+      background-color: black
+</style>
+
+<script>
+// @ is an alias to /src
+import CarrosselDivs from '@/components/CarrosselDivs.vue';
+import dest1 from '@/images/destaques-p/deadpool.jpg';
+import dest2 from '@/images/destaques-p/ea-fc.jpg';
+export default {
+  name: "HomeView",
+  components:{
+    CarrosselDivs,
+  },
+  data(){
+    return{
+      dest1,
+      dest2,
+      nomeExibicao: 'Kyu Hayato',
+      usuarioAtivo: true
+    }
+  }
+};
+
+</script>
 <style>
   header{
     background-color: #190C3C;
@@ -97,41 +161,47 @@
   }
 
   .dest{
-    padding: 10vh 0;
-    background-color: #FCFAFA;
+    padding: 10vh 0 0;
+    border-radius: 0px;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0) 0 0 0;
   }
 
   .destaques{
     width: 80vw;
-    margin: auto;
     padding: 5vh;
-    border-radius: 25px;
+    margin: auto;
+  }
+  
+  section, .expansao{
+    width: 90vw;
+    padding: 5vh;
+    margin: auto;
     background-color: #ffffff;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 10px;
+    border-radius: 25px;
+    margin-bottom: 10vh;
+  }
+
+  footer{
+    text-align: center;
+    background-color: #1D0D46;
+    color: #fff;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 3vh 0;
+  }
+
+  .imgF{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 3cqw;
+  }
+
+  .if{
+    height: 60px;
+    width: auto;
+    margin: 1vw;
+    display: flex;
   }
 </style>
-
-<style lang="sass">
-  #destaquesCarrossel
-    .carousel-indicators li
-      background-color: black
-    .carousel-indicators li.active
-      background-color: black
-</style>
-
-<script>
-// @ is an alias to /src
-import dest1 from '@/images/destaques-p/deadpool.jpg';
-import dest2 from '@/images/destaques-p/ea-fc.jpg';
-export default {
-  name: "HomeView",
-  data(){
-    return{
-      dest1,
-      dest2,
-      nomeExibicao: 'Kyu Hayato',
-      usuarioAtivo: true,
-    }
-  }
-};
-</script>
